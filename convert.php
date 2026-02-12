04ce91b07f335a0b3e73af64e7850d797e7e52df
chaituteja <j.chaitanyateja@gmail.com>
2026-02-12 13:54:30 +1100

    Created array to store conversion rates

--- convert.php
+++ convert.php
@@ -1,5 +1,13 @@
 <?php
 
+$conversionRates = [
+    'AUD' => 1.0,
+    'USD' => 1.5,
+    'GBP' => 1.7,
+    'NZD' => 0.9,
+    'EUR' => 1.5
+];
+
 $amount = $argv[1];
 $from = $argv[2];
 $to = $argv[3];
